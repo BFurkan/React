b5332bc8aa57f97a3f94b3b8dbaf1e25c50053df
Bfurkan <frkanbayr@gmail.com>
2024-02-09 00:46:27 -0500

    First React app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App(props) {
+    const currDate = new Date();
+
+  return (
+    <div>
+   <h1>Hello World</h1>
+   <h2>The time now is {currDate.toLocaleTimeString() + (" ") + currDate.toLocaleDateString()}</h2>
+    </div>
+
+  );
+}
+
+export default App;
