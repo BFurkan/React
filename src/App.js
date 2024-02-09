af4de2a3ce1e5c29348410a8cbae8df647e148fa
Bfurkan <frkanbayr@gmail.com>
2024-02-09 01:00:54 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App(props) {
   return (
     <div>
    <h1>Hello World</h1>
+   <h1>welcome</h1>
    <h2>The time now is {currDate.toLocaleTimeString() + (" ") + currDate.toLocaleDateString()}</h2>
     </div>
 
